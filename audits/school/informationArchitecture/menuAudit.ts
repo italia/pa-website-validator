53e846e2e8aefe049599961dc721168725dae915
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-05-03 14:29:52 +0200

    Aggiornato Audit per verifica menù di primo livello Scuole

--- audits/school/informationArchitecture/menuAudit.ts
+++ audits/school/informationArchitecture/menuAudit.ts
@@ -26,8 +26,8 @@ class LoadAudit extends Audit {
     static get meta() {
         return {
             id: 'school-menu-structure-match-model',
-            title: 'Il menu rispetta le indicazioni fornite dal modello',
-            failureTitle: 'Il menu non rispetta le indicazioni fornite dal modello',
+            title: 'Le voci del menù rispettano il modello',
+            failureTitle: 'Il menu non rispetta le indicazioni fornite dal modello: non sono presenti le voci obbligatorie oppure sono in ordine scorretto prima delle non obbligatorie',
             scoreDisplayMode: Audit.SCORING_MODES.NUMERIC,
             description: 'Test per verificare il rispetto delle regole per la costruzione del menu principale',
             requiredArtifacts: ['menuStructureMatchModel'],
@@ -36,56 +36,38 @@ class LoadAudit extends Audit {
 
     static async audit(artifacts: any) : Promise<{ score: number, details: LH.Audit.Details.Table }> {
         const url = artifacts.menuStructureMatchModel
-        const response = await got(url)
-	    const $ : CheerioAPI = cheerio.load(response.body);
 
         let score = 0
-
-        let headerMenuPassed = false
-
-        const headerUl = $('header').find('ul')
-        let listMatchPrimaryMenuModelObj : primaryModelMenu = {
-            'passed' : false,
-            'rightOrder': false,
-            'items' : [],
-            'rawText' : [],
-            'missingItems': []
-        }
-        let headerFirstLevelMissingItems = []
-        let headerFirstLevelPassedItems = []
-        let headerFirstLevelOrder = false
-        let isFirstUl = true
-
-        for (let ul of headerUl) {
-            const listLi : Cheerio<Element> = $(ul).find('> li > a')
-
-            listMatchPrimaryMenuModelObj = listMatchPrimaryMenuModel(listLi, $)
-            if (listMatchPrimaryMenuModelObj.missingItems.length < headerFirstLevelMissingItems.length || isFirstUl) {
-                isFirstUl = false
-                headerFirstLevelMissingItems = listMatchPrimaryMenuModelObj.missingItems
-                headerFirstLevelPassedItems = listMatchPrimaryMenuModelObj.items
-                headerFirstLevelOrder = listMatchPrimaryMenuModel(listLi, $).rightOrder
-            }
-
-            if (listMatchPrimaryMenuModelObj.passed) {
-                headerMenuPassed = true
-            }
-        }
-
-        if (headerMenuPassed) {
-            score = 1
-        }
-
         const headings = [
-            { key: 'header_missing_first_level_voices', itemType: 'text', text: "Voci di primo livello mancanti (o diverse) nell'header" },
-            { key: 'header_first_level_voices_order', itemType: 'text', text: "Ordine delle voci di primo livello nell'header" },
-            { key: 'model_link', itemType: 'url', text: "Link al modello di riferimento" },
+            { key: 'menu_elements', itemType: 'text', text: "Elementi del menù" },
+            { key: 'required_menu_elements', itemType: 'text', text: "Elementi richiesti (in questo ordine)" },
+            { key: 'required_menu_elements_presence', itemType: 'text', text: "Elementi obbligatori presenti" },
+            { key: 'required_menu_elements_correct_order', itemType: 'text', text: "Elementi obbligatori presenti e ordinati correttamente" },
+            { key: 'model_link', itemType: 'text', text: "Link al modello di riferimento" },
         ]
+
+        const menuItems = JSON.parse(fs.readFileSync(storageFolder + '/' + menuItemsFile))
+        const primaryMenuMandatoryElements = menuItems.primaryMenuItems
+
+        const response = await got(url)
+        const $ = cheerio.load(response.body)
+
+        const menuElements = getMenuElements($)
+        const containsMandatoryElementsResult = containsMandatoryElements(menuElements, primaryMenuMandatoryElements)
+        const mandatoryElementsCorrectOrder = correctOrderMandatoryElements(menuElements, primaryMenuMandatoryElements)
+
+        if (menuElements.length === 4 && containsMandatoryElementsResult && mandatoryElementsCorrectOrder) {
+            score = 1
+        } else if(menuElements.length > 4 && menuElements.length < 7 && containsMandatoryElementsResult && mandatoryElementsCorrectOrder) {
+            score = 0.5
+        }
 
         const items = [
             {
-                header_missing_first_level_voices: headerFirstLevelMissingItems.length === 0 ? 'nessuna' : headerFirstLevelMissingItems.join(', '),
-                header_first_level_voices_order : headerFirstLevelOrder ? 'rispettato' : 'non rispettato',
+                menu_elements: menuElements.join(', '),
+                required_menu_elements: primaryMenuMandatoryElements.join(', '),
+                required_menu_elements_presence: containsMandatoryElementsResult === true ? 'Sì' : 'No',
+                required_menu_elements_correct_order: mandatoryElementsCorrectOrder === true ? 'Sì' : 'No',
                 model_link: 'https://docs.google.com/drawings/d/1qzpCZrTc1x7IxdQ9WEw_wO0qn-mUk6mIRtSgJlmIz7g/edit'
             }
         ]
@@ -99,64 +81,35 @@ class LoadAudit extends Audit {
 
 module.exports = LoadAudit
 
-function listMatchPrimaryMenuModel(list: Cheerio<Element>, $: CheerioAPI) : primaryModelMenu {
-    const menuItems = JSON.parse(fs.readFileSync(storageFolder + '/' + menuItemsFile))
-    const primaryMenuItems = menuItems.primaryMenuItems
+function getMenuElements ($ : CheerioAPI) : string [] {
+    const headerUl = $('#menup').find('> li > a')
 
-    let passedItems : Array<Element> = []
-    let passedRawText : string [] = []
-    let rightOrder = true
-    let count = 0
-    let found = -1
-    for (let i = 0; i < list.length; i++) {
-       for (let j = 0; j < primaryMenuItems.length; j++) {
-            if ($(list[i]).text().trim() == primaryMenuItems[j]) {
-                if (j < found) {
-                    rightOrder = false
-                }
-                found = j
-                passedItems.push(list[i])
-                passedRawText.push(primaryMenuItems[j])
-                count++
-            }
-       }
+    let elements = []
+    for (let element of headerUl) {
+        elements.push($(element).text().trim())
     }
 
-    if (rightOrder == false) {
-        return {
-            'passed' : false,
-            'rightOrder': rightOrder,
-            'items' : passedItems,
-            'rawText' : passedRawText,
-            'missingItems': difference(primaryMenuItems, passedRawText)
-        }
-    }
-
-    if (count >= 4) {
-        return {
-            'passed' : true,
-            'rightOrder': rightOrder,
-            'items' : passedItems,
-            'rawText' : passedRawText,
-            'missingItems': difference(primaryMenuItems, passedRawText)
-        }
-    }
-
-    return {
-      'passed' : false,
-      'rightOrder': rightOrder,
-      'items' : passedItems,
-      'rawText' : passedRawText,
-      'missingItems': difference(primaryMenuItems, passedRawText)
-    }
+    return elements
 }
 
-function difference(array1: string [], array2: string []) : string [] {
-    let result: string [] = []
+function containsMandatoryElements(menuElements: string [], mandatoryElements: string []) : boolean {
+    let result = true
 
-    for (let a of array1) {
-        if (!array2.includes(a)) {
-            result.push(a)
+    for (let element of mandatoryElements) {
+        if (!menuElements.includes(element)) {
+            result  = false
+        }
+    }
+
+    return result
+}
+
+function correctOrderMandatoryElements(menuElements: string [], mandatoryElements: string []) : boolean {
+    let result = true
+
+    for (let i = 0; i < mandatoryElements.length; i++) {
+        if (menuElements[i] !== mandatoryElements[i]) {
+            result = false
         }
     }
 
