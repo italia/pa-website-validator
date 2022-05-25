371b967f795bc4b9eb322fab735c2e062fcdc75a
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-05-25 14:56:21 +0200

    fix: Modificata index del comando per integrare la nuova logica di lancio lighthouse centralizzata ad oggetti

---
+++ controller/launchLighthouse.ts
@@ -0,0 +1,84 @@
+'use strict'
+
+//@ts-ignore
+import lighthouse from "lighthouse"
+
+import * as fs from "fs"
+import * as chromeLauncher from "chrome-launcher"
+
+import * as schoolOnlineConfig from "../config/school/auditConfig-online"
+import * as schoolLocalConfig from "../config/school/auditConfig-local"
+import * as municipalityOnlineConfig from "../config/municipality/auditConfig-online"
+import * as municipalityLocalConfig from "../config/municipality/auditConfig-local"
+
+import { RunnerResult } from "lighthouse/types/externs"
+
+export const logLevels = {
+    display_none: "silent",
+    display_error: "error",
+    display_info: "info",
+    display_verbose: "verbose"
+}
+
+const run = async (website: string, type: string, scope: string, destination: string, reportName: string, logLevel: string = logLevels.display_none) => {
+    //L'oggetto chrome non è incluso nel try-catch in modo tale che la sua istanza venga killata anche in caso di eccezione lanciata da altri processi
+    const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']})
+
+    try {
+
+        let loadConfig
+        if (type === 'school' && scope === 'local') {
+            loadConfig = schoolLocalConfig
+        } else if (type === 'school' && scope === 'online') {
+            loadConfig = schoolOnlineConfig
+        } else if (type === 'municipality' && scope === 'local') {
+            loadConfig = municipalityLocalConfig
+        } else if (type === 'municipality' && scope === 'online') {
+            loadConfig = municipalityOnlineConfig
+        } else {
+            throw new Error('Wrong type or scope passe - No config to load')
+        }
+
+        const options = { logLevel: logLevel, output: ['html', 'json'], port: chrome.port, locale: "it" }
+        const runnerResult: RunnerResult = await lighthouse(website, options, loadConfig)
+
+        if (runnerResult.report.length != 2) {
+            throw new Error('Missing JSON or HTML report')
+        }
+
+        const reportHtml: string = runnerResult.report[0]
+        const reportJSON: string = runnerResult.report[1]
+
+        if (!fs.existsSync(destination)){
+            console.log("[WARNING] Directory does not exist..")
+            fs.mkdirSync(destination, {recursive: true})
+            console.log("[INFO] Directory created at: " + destination)
+        }
+
+        const htmlPath: string = destination + '/' + reportName + '.html'
+        const jsonPath: string = destination + '/' + reportName + '.json'
+        fs.writeFileSync(htmlPath, reportHtml)
+        fs.writeFileSync(jsonPath, reportJSON)
+
+        await chrome.kill()
+
+        return {
+            status: true,
+            data: {
+                htmlResultPath: htmlPath,
+                jsonResultPath: jsonPath,
+            }
+        }
+    } catch (ex) {
+        await chrome.kill()
+
+        return {
+            status: false,
+            data: {
+                exception: ex
+            }
+        }
+    }
+}
+
+export { run }
--- index.ts
+++ index.ts
@@ -1,43 +1,26 @@
 #!/usr/bin/env node
 
-import { spawn } from "child_process"
+import { run, logLevels } from "./controller/launchLighthouse"
 import * as fs from "fs"
 import yargs from "yargs"
 
 const crawelerCommand = yargs
-    .usage("Usage: --type <type> --destination <folder> --report <report_name> --website <url> --scope <scope> --view <view>")
+    .usage("Usage: --type <type> --destination <folder> --report <report_name> --website <url> --scope <scope>")
     .option("type", { describe: "Crawler to run", type: "string", demandOption: true , choices: ['municipality', 'school']})
     .option("destination", { describe: "Path where to save the report", type: "string", demandOption: true })
     .option("report", { describe: "Name of the result report", type: "string", demandOption: true })
     .option("website", { describe: "Website where to run the crawler", type: "string", demandOption: true })
     .option("scope", { describe: "Execution scope", type: "string", demandOption: true , default: "online", choices: ['local', 'online']})
-    .option("view", { describe: "Autolaunch reports in browser", type: "string", demandOption: true , default: "no", choices: ['yes', 'no']})
     .parseSync();
 
-let configPath = __dirname + `/config/${crawelerCommand.type}/auditConfig-${crawelerCommand.scope}.js`
+if (!fs.existsSync(crawelerCommand.destination)){
+    console.log("[WARNING] Directory does not exist..")
+    fs.mkdirSync(crawelerCommand.destination, {recursive: true})
+    console.log("[INFO] Directory created at: " + crawelerCommand.destination)
+}
 
-console.log('[INFO] Loaded config: ' + configPath)
-
-fs.access(crawelerCommand.destination, function(error) {
-    if (error) {
-        console.log("[WARNING] Directory does not exist..")
-        fs.mkdirSync(crawelerCommand.destination, { recursive: true })
-        console.log("[INFO] Directory created at: " + crawelerCommand.destination)
-    }
-})
-
-const savePath = crawelerCommand.destination + '/' + crawelerCommand.report
-
-const view = crawelerCommand.view === 'yes' ? '--view' : ''
-console.log('[INFO] Try to execute: ' + `node ${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js --locale it --config-path=${configPath} --chrome-flags="--headless" --output json --output html --output-path ${savePath} ${crawelerCommand.website} ${view}`)
-const child = spawn(`npx lighthouse --locale it --config-path=${configPath} --chrome-flags="--headless" --output json --output html --output-path ${savePath} ${crawelerCommand.website} ${view}`, {
-    shell: true
-})
-
-child.stderr.on('data', (data) => {
-    process.stdout.write('[INFO]: ' + data)
-});
-
-child.on('close', (code) => {
-    console.log(`Process exited with code ${code}`)
+Promise.resolve(run(crawelerCommand.website, crawelerCommand.type, crawelerCommand.scope, crawelerCommand.destination, crawelerCommand.report, logLevels.display_info)).then((result) => {
+    console.log('[INFO] Status result:', result.status)
+    console.log('[INFO] HTML report file:', result.data.htmlResultPath)
+    console.log('[INFO] JSON report file:', result.data.jsonResultPath)
 })
