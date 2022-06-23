aac4eef0d17a7dc5d705cb1ac87de50bdd29c371
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-06-23 11:15:12 +0200

    fix: Aggiornate label audit bootstrap italia

--- audits/common/bootstrapItaliaDoubleCheckAudit.ts
+++ audits/common/bootstrapItaliaDoubleCheckAudit.ts
@@ -7,22 +7,22 @@ import lighthouse from "lighthouse";
 const Audit = lighthouse.Audit;
 
 const greenResult =
-  "Il sito utilizza la libreria Bootstrap Italia in una versione più recente o uguale di 1.6.3";
+  "Il sito utilizza la libreria Bootstrap Italia in una versione idonea.";
 const redResult =
-  "Il sito non utilizza la libreria Bootstrap Italia o ne utilizza una versione precedente a 1.6.3";
-const libraryName = "Bootstrap-italia";
+  "Il sito non utilizza la libreria Bootstrap Italia o ne utilizza una versione datata.";
+const libraryName = "Bootstrap italia";
 
 class LoadAudit extends Audit {
   static get meta() {
     return {
       id: "school-ux-ui-consistency-bootstrap-italia-double-check",
       title:
-        "LIBRERIA DI ELEMENTI DI INTERFACCIA - Il sito scuola deve utilizzare la libreria Bootstrap Italia.",
+        "LIBRERIA DI ELEMENTI DI INTERFACCIA - Il sito deve utilizzare la libreria Bootstrap Italia.",
       failureTitle:
-        "LIBRERIA DI ELEMENTI DI INTERFACCIA - Il sito scuola deve utilizzare la libreria Bootstrap Italia.",
+        "LIBRERIA DI ELEMENTI DI INTERFACCIA - Il sito deve utilizzare la libreria Bootstrap Italia.",
       scoreDisplayMode: Audit.SCORING_MODES.BINARY,
       description:
-        "CONDIZIONI DI SUCCESSO: la versione di libreria Bootstrap Italia in uso è uguale o superiore alla 1.6; MODALITÀ DI VERIFICA: viene verificata la presenza della libreria Bootstrap Italia e la versione in uso individuando la variabile window.BOOTSTRAP_ITALIA_VERSION della libreria; RIFERIMENTI TECNICI E NORMATIVI: [Docs Italia, documentazione Modello Scuole.](https://docs.italia.it/italia/designers-italia/design-scuole-docs/it/v2022.1/index.html)",
+        "CONDIZIONI DI SUCCESSO: la versione di libreria Bootstrap Italia in uso è uguale o superiore alla 1.6.3; MODALITÀ DI VERIFICA: viene verificata la presenza della libreria Bootstrap Italia e la versione in uso individuando la proprietà CSS --bootstrap-italia-version all’interno del selettore :root, o la variabile globale window.BOOTSTRAP_ITALIA_VERSION; RIFERIMENTI TECNICI E NORMATIVI: [Docs Italia, documentazione Modello Scuole.](https://docs.italia.it/italia/designers-italia/design-scuole-docs/it/v2022.1/index.html)",
       requiredArtifacts: [
         "bootstrapItaliaSelectorCheck",
         "bootstrapItaliaCheck",
