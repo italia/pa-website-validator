3ab6fa0b4d6e5510fcfa1713e9e95d17ed49d230
Nicola Squartini <tensor5@gmail.com>
2022-07-27 01:32:40 +0900

    feat: improve app text messages

--- app.ts
+++ app.ts
@@ -4,33 +4,49 @@ import { run, logLevels } from "./controller/launchLighthouse.js";
 
 const type = process.argv[2];
 
-try {
-  const { url, scope }: { url: string; scope: "local" | "online" } =
-    await inquirer.prompt([
-      {
-        message: "URL da scansionare?",
-        name: "url",
-        validate: (input: string) =>
-          input.match(/https?:\/\//) ? true : "URL non valido",
-      },
-      {
-        message: "In quale ambiente stai convalidando il tuo sito?",
-        name: "scope",
-        type: "list",
-        choices: [
-          {
-            name: "Ambiente locale (es. http://localhost:8080)",
-            value: "local",
-          },
-          {
-            name: "Ambiente di produzione (es. https://example.com)",
-            value: "online",
-          },
-        ],
-      },
-    ]);
+const typeIT = type === "school" ? "scuola" : "comune";
 
-  const result = await run(
+const urlSchoolMsg =
+  "Inserisci la URL della pagina principale (homepage) del sito web della scuola:";
+const urlMunicipalityMsg =
+  "Inserisci la URL della pagina principale (homepage) del sito web del Comune:";
+
+console.log();
+console.log();
+
+try {
+  const { url }: { url: string } = await inquirer.prompt([
+    {
+      message: type === "school" ? urlSchoolMsg : urlMunicipalityMsg,
+      name: "url",
+      validate: (input: string) =>
+        input.match(/https?:\/\//) ? true : "URL non valido",
+    },
+  ]);
+
+  console.log();
+
+  const { scope }: { scope: "local" | "online" } = await inquirer.prompt([
+    {
+      message:
+        "In quale ambiente si trova il sito su cui vuoi condurre l'analisi?",
+      name: "scope",
+      type: "list",
+      choices: [
+        {
+          name: "Ambiente locale (sito in fase di sviluppo, es. di URL: http://localhost:8080)",
+          value: "local",
+        },
+        {
+          name: `Ambiente di produzione (sito online e pubblico, es. di URL: https://${typeIT}.it)`,
+          value: "online",
+        },
+      ],
+    },
+  ]);
+
+  console.log();
+  await run(
     url,
     type,
     scope,
@@ -41,9 +57,14 @@ try {
     true
   );
 
-  console.log("[INFO] Status result:", result.status);
-  console.log("[INFO] HTML report file:", result.data.htmlResultPath);
-  console.log("[INFO] JSON report file:", result.data.jsonResultPath);
+  console.log();
+  console.log();
+  console.log(
+    "[--L'analisi è terminata. Il report si aprirà automaticamente nel tuo browser--]"
+  );
+  console.log(
+    `Se non dovesse aprirsi, una copia HTML e JSON del report è stata salvata nella cartella ${process.cwd()}`
+  );
 } catch (e) {
   console.error(e);
   process.exit(1);
