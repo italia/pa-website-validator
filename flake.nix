36531080fa2fa44edf443fbba5210cd5d472d7fe
Nicola Squartini <tensor5@gmail.com>
2022-09-28 14:06:14 +0200

    chore: add direnv configuration

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Tool di validazione per i siti di comuni e scuole";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { flake-utils, nixpkgs, self }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cocogitto
+            pkgs.nodejs-16_x
+            pkgs.pandoc
+            pkgs.shellcheck
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
