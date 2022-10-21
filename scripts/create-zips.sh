27f39faa606333e10cf1943cc18773db258a6352
Nicola Squartini <tensor5@gmail.com>
2022-10-21 10:46:57 +0200

    fix: do not include extra root folder in zip files

--- scripts/create-zips.sh
+++ scripts/create-zips.sh
@@ -29,15 +29,15 @@ fi
 for i in 0 1; do
   type_en=${types_en[$i]}
   type_it=${types_it[$i]}
-  dir=$type_it-$version-$os-$arch
-  mkdir "$dir"
+  exe_filename=app-valutazione-$type_it$ext
+  readme_filename=README.html
+  zip_filename=$type_it-$version-$os-$arch.zip
   npm run "bundle:$type_en$build_variant"
-  mv "app-valutazione-$type_it$ext" "$dir"
-  cp "docs/README-$type_it-$os.html" "$dir/README.html"
+  cp "docs/README-$type_it-$os.html" $readme_filename
   if [ $is_win == 0 ]; then
-    zip -r "$dir.zip" "$dir"
+    zip "$zip_filename" "$exe_filename" $readme_filename
   else
-    7z a -tzip "$dir.zip" "$dir"
+    7z a -tzip "$zip_filename" "$exe_filename" $readme_filename
   fi
-  rm -r "$dir"
+  rm "$exe_filename" $readme_filename
 done
