23be6c4c133cd288b85765094e50357134cfbe89
Nicola Squartini <tensor5@gmail.com>
2022-12-09 17:03:14 +0100

    test: fix expected results

--- tests/municipality.ts
+++ tests/municipality.ts
@@ -29,8 +29,8 @@ type ExpectedLocalEntry = [
 ];
 
 const expectedLocal: Array<ExpectedLocalEntry> = [
-  ["2.0.2", [1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0.5, 1]],
-  ["test01", [0, 1, 1, 0, 0, 1, 0.5, 1, 1, 0.5, 0, 1, 1, 0.5, 1]],
+  ["2.0.2", [1, 0.5, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0.5, 0.5, 0]],
+  ["test01", [0, 0.5, 1, 0, 0, 1, 0.5, 1, 1, 0.5, 0, 1, 0, 0.5, 0]],
 ];
 
 type ExpectedOnlineEntry = [
