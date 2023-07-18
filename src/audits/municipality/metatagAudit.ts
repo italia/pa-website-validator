1dffa8b969b663f002c185c71418eb470b67cb8a
simone.amadio <simone.amadio@accenture.com>
2023-07-18 10:37:09 +0200

    fix: change audience.name in audience.audienceType

--- src/audits/municipality/metatagAudit.ts
+++ src/audits/municipality/metatagAudit.ts
@@ -276,7 +276,7 @@ const metatadaJSONStructure = {
     audience: {
       type: "object",
       properties: {
-        name: { type: "string", minLength: 1 },
+        audienceType: { type: "string", minLength: 1 },
       },
 
       required: ["name"],
