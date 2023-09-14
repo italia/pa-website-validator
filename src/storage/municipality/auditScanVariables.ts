df82b3f3fb860562a177928ae1a4551227b0949f
simone.amadio <simone.amadio@accenture.com>
2023-09-14 11:28:17 +0200

    feat: add primary, secondary and service pages in domain audit

--- src/storage/municipality/auditScanVariables.ts
+++ src/storage/municipality/auditScanVariables.ts
@@ -35,6 +35,11 @@ export const auditScanVariables = {
     "municipality-booking-appointment-check": {
       numberOfServicesToBeScanned: 1,
     },
+    "municipality-domain":{
+      numberOfFirstLevelPageToBeScanned: 1,
+      numberOfSecondLevelPageToBeScanned: 1,
+      numberOfServicesToBeScanned: 1,
+    }
   },
   suggested: {
     "municipality-ux-ui-consistency-bootstrap-italia-double-check": {
@@ -72,6 +77,11 @@ export const auditScanVariables = {
     "municipality-metatag": {
       numberOfServicesToBeScanned: 5,
     },
+    "municipality-domain":{
+      numberOfFirstLevelPageToBeScanned: 5,
+      numberOfSecondLevelPageToBeScanned: 5,
+      numberOfServicesToBeScanned: 5,
+    }
   },
   high: {
     "municipality-ux-ui-consistency-bootstrap-italia-double-check": {
@@ -109,6 +119,11 @@ export const auditScanVariables = {
     "municipality-booking-appointment-check": {
       numberOfServicesToBeScanned: 10,
     },
+    "municipality-domain":{
+      numberOfFirstLevelPageToBeScanned: 10,
+      numberOfSecondLevelPageToBeScanned: 10,
+      numberOfServicesToBeScanned: 10,
+    }
   },
   all: {
     "municipality-ux-ui-consistency-bootstrap-italia-double-check": {
@@ -146,5 +161,10 @@ export const auditScanVariables = {
     "municipality-metatag": {
       numberOfServicesToBeScanned: -1,
     },
+    "municipality-domain":{
+      numberOfFirstLevelPageToBeScanned: -1,
+      numberOfSecondLevelPageToBeScanned: -1,
+      numberOfServicesToBeScanned: -1,
+    }
   },
 };
