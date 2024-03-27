8ffdfa9d7ef29cd93248aadf8ee5a7d3b32a6744
Nicola Squartini <tensor5@gmail.com>
2024-03-27 13:48:15 +0100

    fix: accept `'` character in legal notes

--- src/storage/common/legalNotes.ts
+++ src/storage/common/legalNotes.ts
@@ -8,6 +8,6 @@ export const legalNotes = {
     dataElement: "legal-notes-body",
     text:
       "In applicazione del principio open by default ai sensi dell’articolo 52 del decreto legislativo 7 marzo 2005, n. 82 (CAD) e salvo dove diversamente specificato (compresi i contenuti incorporati di terzi), i dati, i documenti e le informazioni pubblicati sul sito sono rilasciati con licenza CC-BY 4.0. " +
-      "Gli utenti sono quindi liberi di condividere (riprodurre, distribuire, comunicare al pubblico, esporre in pubblico), rappresentare, eseguire e recitare questo materiale con qualsiasi mezzo e formato e modificare (trasformare il materiale e utilizzarlo per opere derivate) per qualsiasi fine, anche commerciale con il solo onere di attribuzione, senza apporre restrizioni aggiuntive. ",
+      "Gli utenti sono quindi liberi di condividere (riprodurre, distribuire, comunicare al pubblico, esporre in pubblico), rappresentare, eseguire e recitare questo materiale con qualsiasi mezzo e formato e modificare (trasformare il materiale e utilizzarlo per opere derivate) per qualsiasi fine, anche commerciale con il solo onere di attribuzione, senza apporre restrizioni aggiuntive.",
   },
 };
